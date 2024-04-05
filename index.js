55a77c3235fc56dc60176fc38d741cb83f644506
ABDUL REHMAN <163383150+AbdulRehman226@users.noreply.github.com>
2024-04-05 16:47:11 +0500

    my first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+import inquirer from "inquirer";
+let todos = [];
+let condition = true;
+while (condition) {
+    let addTask = await inquirer.prompt([
+        {
+            name: "todos",
+            type: "input",
+            message: "What you want to add in your todos? ",
+        },
+        {
+            name: "addMore",
+            type: "confirm",
+            message: "Do you want to add more in your todos ?",
+            default: "true",
+        },
+    ]);
+    todos.push(addTask.todos);
+    condition = addTask.addMore;
+    console.log(todos);
+}
